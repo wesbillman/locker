31e1be10a3f63c13e547437a52ce8f79b9c02d7d
Wes Billman <wes@compass.com>
2019-10-06 17:15:07 -0600

    Starting to build up CLI

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-module.exports = () => {
-    console.log('Welcome to the outside!')
+import program from 'commander'
+import chalk from 'chalk'
+import yaml from 'js-yaml'
+import fs from 'fs'
+import os from 'os'
+
+const log = console.log;
+
+program
+  .option('-f, --file <path>', 'Lockerfile path')
+
+
+function local(path) {
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path)
+    log(chalk.green(`${path} created for local storage`))
   }
+}
+
+export function cli(args) {
+  program.parse(args)
+
+  let lockerfile = 'Lockerfile'
+  if (program.file) {
+    lockerfile = program.file
+  }
+
+  if (!fs.existsSync(lockerfile)) {
+    log(chalk.red(`Lockerfile not found at ${lockerfile}`))
+    process.exit(1)
+  }
+
+  let doc
+  try {
+    doc = yaml.safeLoad(fs.readFileSync(lockerfile, 'utf8'));
+  } catch (e) {
+    log(e);
+  }
+
+  if (doc.rooms.local) {
+    const localPath = doc.rooms.local.replace(/^~/, os.homedir());
+    local(localPath);
+  }
+}
