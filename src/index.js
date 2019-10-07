0d6280ff459c6bbb7d5d060971ed5c3894706f6c
Wes Billman <wes@compass.com>
2019-10-06 19:30:25 -0600

    Added push and pull commands

--- src/index.js
+++ src/index.js
@@ -1,25 +1,82 @@
 import program from 'commander'
 import chalk from 'chalk'
 import yaml from 'js-yaml'
-import fs from 'fs'
+import fs from 'fs-extra'
 import os from 'os'
 
 const log = console.log;
 
-program
-  .option('-f, --file <path>', 'Lockerfile path')
+program.version('0.0.1')
+program.command('pull')
+  .description('Pull down dependecies from your lockers')
+  .action(() => {
+    localPull(getDoc())
+  })
+program.command('push')
+  .description('Push up local changes to your lockers')
+  .action(() => {
+    localPush(getDoc())
+  })
+program.option('-f, --file <path>', 'Lockerfile path')
+program.on('command:*', function () {
+  console.error('Invalid command: %s\nSee --help for a list of available commands.', program.args.join(' '));
+  process.exit(1);
+})
 
+function validate(doc) {
+  if (!doc.team) {
+    log(chalk.red('Lockerfile must specify a "team"'))
+    process.exit(1)
+  }
 
-function local(path) {
-  if (!fs.existsSync(path)) {
-    fs.mkdirSync(path)
-    log(chalk.green(`${path} created for local storage`))
+  if (!doc.lockers) {
+    log(chalk.red('Lockerfile must specify at least one "locker"'))
+    process.exit(1)
+  }
+
+  if (!doc.gear) {
+    log(chalk.red('Lockerfile must specify "gear"'))
+    process.exit(1)
   }
 }
 
-export function cli(args) {
-  program.parse(args)
+function localPull(doc) {
+  log(chalk.blue('Getting gear out of your locker'))
+  let localPath = getLocalPath(doc)
+  if (!localPath) {
+    return
+  }
 
+  doc.gear.directories.forEach(element => {
+    log(`dir  - ${chalk.green(element)}`)
+  });
+
+  doc.gear.files.forEach(element => {
+    log(`file - ${chalk.green(element)}`)
+  });
+}
+
+function localPush(doc) {
+  log(chalk.blue('Putting gear in your locker'))
+  let localPath = getLocalPath(doc)
+  if (!localPath) {
+    return
+  }
+
+  doc.gear.directories.forEach(element => {
+    process.stdout.write(`putting ${chalk.green(element)} in your locker...`)
+    fs.copySync(element, `${localPath}/${element}`)
+    log('✅')
+  });
+
+  doc.gear.files.forEach(element => {
+    process.stdout.write(`putting ${chalk.green(element)} in your locker...`)
+    fs.copySync(element, `${localPath}/${element}`)
+    log('✅')
+  });
+}
+
+function getDoc() {
   let lockerfile = 'Lockerfile'
   if (program.file) {
     lockerfile = program.file
@@ -32,13 +89,24 @@ export function cli(args) {
 
   let doc
   try {
-    doc = yaml.safeLoad(fs.readFileSync(lockerfile, 'utf8'));
+    return yaml.safeLoad(fs.readFileSync(lockerfile, 'utf8'));
   } catch (e) {
     log(e);
   }
 
-  if (doc.rooms.local) {
-    const localPath = doc.rooms.local.replace(/^~/, os.homedir());
-    local(localPath);
+  validate(doc)
+}
+
+function getLocalPath(doc) {
+  let localPath = doc.lockers.local.replace(/^~/, os.homedir());
+  localPath = `${localPath}/${doc.team}`
+  if (!fs.existsSync(localPath)) {
+    fs.mkdirSync(localPath)
+    log(chalk.green(`${localPath} created for local storage`))
   }
+  return localPath
+}
+
+export function cli(args) {
+  program.parse(args)
 }
